e965c26bc99120219d283625a7a3dc1b4274d326
matias-crypto <149725150+matias-crypto@users.noreply.github.com>
2024-01-12 02:54:20 -0300

    Create descargador.js

---
+++ plugins/descargador.js
@@ -0,0 +1,17 @@
+{
+    "code": 0,
+    "message": "success",
+    "ttl": 1,
+    "data": {
+        "url_list": [
+            {
+                "id": 0,
+                "url": "https://sf16-ies-music-va.tiktokcdn.com/obj/musically-maliva-obj/7111177662001515269.mp3",
+                "size": 3644692,
+                "duration": 230.528,
+                "format": "mp3",
+                "bitrate": 128
+            }
+        ]
+    }
+      }
